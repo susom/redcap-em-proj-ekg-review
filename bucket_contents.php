21496866c8c2876540683c1fd91e2904ae80a94c
Andrew Martin <andy123@stanford.edu>
2018-11-26 16:43:46 -0800

    added folder support to bucket_contents and recordset_creator

--- bucket_contents.php
+++ bucket_contents.php
@@ -1,22 +1,50 @@
 <?php
 /** @var \Stanford\EkgReview\EkgReview $module */
 
-
 require_once APP_PATH_DOCROOT . 'ProjectGeneral/header.php';
 
-// Display records in bucket
-$contents = $module->getBucketContents(["prefix" => "adjudication/"]);
-
+$raw_bucket_prefix = isset($_POST['bucket_prefix']) ? $_POST['bucket_prefix'] : "";
+$bucket_prefix = mb_ereg_replace("([^\w\s\d\-_~,;\[\]\(\).])", '', $raw_bucket_prefix);
 $bucket_name = $module->getProjectSetting('gcp-bucket-name');
 
-// DEBUG
 ?>
-<hr>
-<h4>Bucket Contents (<?php echo $bucket_name?>) <span class='badge badge-secondary'><?php echo count($contents) ?></span>
-    <button class="btn btn-xs btn-primary" type="button" data-toggle="collapse" data-target="#collapseContents" aria-expanded="false" aria-controls="collapseContents">
-        Toggle Contents
-    </button>
-</h4>
-<div class="collapse show" id="collapseContents">
-    <pre><?php echo implode("\n", $contents) ?></pre>
-</div>
+
+    <h4>GCP Bucket for serving images: <?php echo $bucket_name ?></h4>
+    <form method="post">
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <span class="input-group-text"><i class="fab fa-bitbucket mr-2"></i> Bucket folder containing files: </span>
+                <input type="text" class="form-control" placeholder="e.g. group1"
+                       name="bucket_prefix" value="<?php echo $bucket_prefix ?>" aria-describedby="bucket_prefix_label">
+                <span class="input-group-btn">
+                <button type="submit" name="action" value="set" class="btn btn-primaryrc">Scan</button>
+            </span>
+            </div>
+        </div>
+
+    </form>
+
+<?php
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // Display records in bucket
+    $contents = $module->getBucketContents(["prefix" => $bucket_prefix . "/"]);
+
+    ?>
+    <hr>
+    <h4><?php echo $bucket_prefix ?> Contents: <span class='badge badge-secondary'><?php echo count($contents) ?></span> files
+        <button class="btn btn-xs btn-primary" type="button" data-toggle="collapse" data-target="#collapseContents" aria-expanded="false" aria-controls="collapseContents">
+            Toggle Contents
+        </button>
+    </h4>
+    <div class="collapse show" id="collapseContents">
+        <pre><?php echo implode("\n", $contents) ?></pre>
+    </div>
+    <?php
+
+    REDCap::logEvent("EKG Bucket Contents", "Found " . count($contents) . " files in $bucket_prefix folder of $bucket_name");
+}
+
+
--- recordset_creator.php
+++ recordset_creator.php
@@ -30,61 +30,179 @@ function convertArrayToCsv($array) {
     return stream_get_contents($csv);
 }
 
-// Get bucket contents
-$bucket_contents = $module->getBucketContents(["prefix" => "adjudication/"]);
 
+
+
+
+
+
+
+
+
+require_once APP_PATH_DOCROOT . 'ProjectGeneral/header.php';
+
+
+?>
+
+<style>
+    .col-form-label {
+        font-size: 14pt;
+    }
+</style>
+
+<?php
+
+$bucket_name = $module->getProjectSetting('gcp-bucket-name');
+
+$raw_bucket_prefix = isset($_POST['bucket_prefix']) ? $_POST['bucket_prefix'] : "";
+$bucket_prefix = mb_ereg_replace("([^\w\s\d\-_~,;\[\]\(\).])", '', $raw_bucket_prefix);
+
+?>
+    <h4>Recordset Creator: <?php echo $bucket_name . " " . $bucket_prefix ?></h4>
+<?php
+
+// We have a bucket prefix - get contents
+$bucket_contents = $module->getBucketContents(["prefix" => $bucket_prefix . "/"]);
+
+
+// Parse post if available
+$f_reviewers = !empty($_POST['f_reviewers']) ? $_POST['f_reviewers'] : "a,b,c,d,e,f,g,h,i,j,k,l";
+$f_versions = !empty($_POST['f_versions']) ? $_POST['f_versions'] : "1,2";
+$f_iapr = !empty($_POST['f_iapr']) ? intval($_POST['f_iapr']) : 800;
+$f_iqcpr = !empty($_POST['f_iqcpr']) ? intval($_POST['f_iqcpr']) : 80;
+$f_mapr = !empty($_POST['f_mapr']) ? $_POST['f_mapr'] : 400;
+$f_oc = !empty($_POST['f_oc']) ? $_POST['f_oc'] : count($bucket_contents);
+
+// Render input form
+?>
+
+<form method="post">
+
+    <div class="form-group row">
+        <label class="col-sm-2 col-form-label" for="f_oc"><i class="fab fa-bitbucket mr-2"></i> Bucket Folder</label>
+        <div class="input-group-prepend col-sm-10">
+            <input type="text" class="form-control" id="bucket_prefix" name="bucket_prefix" placeholder="Name of folder in the bucket to scan"
+                   value="<?php echo $bucket_prefix ?>">
+            <span class="input-group-btn">
+                <button type="submit" name="scan" value="scan" class="btn btn-success">Scan</button>
+            </span>
+        </div>
+    </div>
+
+    <?php if (!empty($bucket_prefix)) { ?>
+
+        <div class="form-group row pt-2">
+            <label class="col-sm-2 col-form-label" for="f_oc">CSV Objects</label>
+            <div class="col-sm-10">
+                <input type="number" class="form-control" id="f_oc" name="f_oc" placeholder=""
+                       value="<?php echo $f_oc ?>" max="<?php echo count($bucket_contents) ?>">
+                <small class="form-text text-muted">Number of EKG objects to use for this data set.  Defaults to count in bucket and can't be more than bucket count of <?php echo count($bucket_contents) ?></small>
+            </div>
+        </div>
+
+        <div class="form-group row pt-2">
+            <label class="col-sm-2 col-form-label" for="f_reviewers">Reviewers</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="f_reviewers" name="f_reviewers" placeholder="a,b,c,d,..."
+                       value="<?php echo $f_reviewers ?>">
+                <small class="form-text text-muted">Enter DAGs as comma-separated list: e.g. a,b,c,d</small>
+            </div>
+        </div>
+
+        <div class="form-group row pt-2">
+            <label class="col-sm-2 col-form-label" for="f_versions">Versions</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="f_versions" name="f_versions" placeholder="1,2"
+                       value="<?php echo $f_versions ?>">
+                <small class="form-text text-muted">Enter versions to create for each file as a comma-separated list: e.g. 1,2</small>
+            </div>
+        </div>
+
+        <div class="form-group row pt-2">
+            <label class="col-sm-2 col-form-label" for="f_iapr">Initial Allocation</label>
+            <div class="col-sm-10">
+                <input type="number" class="form-control" id="f_iapr" name="f_iapr" placeholder=""
+                       value="<?php echo $f_iapr ?>">
+                <small class="form-text text-muted">Number of records to initially allocate to each reviewer (remainder, if any, will be unallocated)</small>
+            </div>
+        </div>
+
+        <div class="form-group row pt-2">
+            <label class="col-sm-2 col-form-label" for="f_mapr">Maximum Version 1 Records</label>
+            <div class="col-sm-10">
+                <input type="number" class="form-control" id="f_mapr" name="f_mapr" placeholder=""
+                       value="<?php echo $f_mapr ?>">
+                <small class="form-text text-muted">Maximum number of version 1 objects to initially assign to a reviewer. This is used to ensure equal adjudication across users.  Recommended value is Initial Allocation / # versions</small>
+            </div>
+        </div>
+
+        <div class="form-group row pt-2">
+            <label class="col-sm-2 col-form-label" for="f_iqcpr">Initial QC Repeats</label>
+            <div class="col-sm-10">
+                <input type="number" class="form-control" id="f_iqcpr" name="f_iqcpr" placeholder=""
+                       value="<?php echo $f_iqcpr ?>">
+                <small class="form-text text-muted">Number of EKGs to repeat from initial allocation of version 1 records for the SAME reviewer (i.e. versions 99s).  Cannot be more than Max Ver 1 records.</small>
+            </div>
+        </div>
+
+        <button type="submit" name="generate" value="generate" class="w-100 btn btn-primaryrc">Generate</button>
+
+    <?php } ?>
+
+</form>
+
+<?php
+
+// Quit if nothing else to do.
+if (empty($_POST['generate'])) exit();
 
 $debug   = [];
 $alert   = [];
 $missing = [];
 
+$reviewers = array_map("trim", explode(",", $f_reviewers));
+$versions = array_map("intval", explode(",", $f_versions));
+$object_count = $f_oc;
 
-$reviewers                                  = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l"
-];
+$initial_allocation_per_reviewer = $f_iapr;
+$initial_qc_per_reviewer = $f_iqcpr;
+$max_initial_allocation_per_version = $f_mapr;
 
-//$initial_allocation_per_reviewer            = 800;   // Number of records to allocate to each reviewer (before QC)
-//$max_initial_allocation_per_version         = 400;    // Max number of records for any given version to be allocated ( use 1/2 of $initial_allocation for 2 versions)
-//$initial_qc_per_reviewer                    = 80;    // Number of version 1 records to QC per reviewer
-//$object_start                               = 1; // Just a starting number - could be anything
-//$object_count                               = 6000;   // How many objects to make for this dataset
-//$versions                                   = range(1,2);
+if (empty($reviewers)) $alert[] = "Missing required reviewers";
+if (empty($versions)) $alert[] = "Missing required versions";
 
 
-$versions                                   = range(1,2);
-$initial_allocation_per_reviewer            = 600;   // Number of records to allocate to each reviewer (before QC)
-$max_initial_allocation_per_version         = 300;    // Max number of records for any given version to be allocated ( use 1/2 of $initial_allocation for 2 versions)
-$initial_qc_per_reviewer                    = 60;    // Number of version 1 records to QC per reviewer
-//$object_start                               = 1000; // Just a starting number - could be anything
-$object_count                               = 10000;   // How many different objects to use (will end up record count =  $versions * $object_count + QC
+    //$initial_allocation_per_reviewer            = 800;   // Number of records to allocate to each reviewer (before QC)
+    //$max_initial_allocation_per_version         = 400;    // Max number of records for any given version to be allocated ( use 1/2 of $initial_allocation for 2 versions)
+    //$initial_qc_per_reviewer                    = 80;    // Number of version 1 records to QC per reviewer
+    //$object_start                               = 1; // Just a starting number - could be anything
+    //$object_count                               = 6000;   // How many objects to make for this dataset
+    //$versions                                   = range(1,2);
+
+
+    //$versions                                   = range(1,2);
+    //$initial_allocation_per_reviewer            = 600;   // Number of records to allocate to each reviewer (before QC)
+    //$max_initial_allocation_per_version         = 300;    // Max number of records for any given version to be allocated ( use 1/2 of $initial_allocation for 2 versions)
+    //$initial_qc_per_reviewer                    = 60;    // Number of version 1 records to QC per reviewer
+    ////$object_start                               = 1000; // Just a starting number - could be anything
+    //$object_count                               = 10000;   // How many different objects to use (will end up record count =  $versions * $object_count + QC
 
 
 if (isset($_GET['test'])) {
     $reviewers                                  = ["a", "b", "c"];
     $versions                                   = range(1,2);
     $initial_allocation_per_reviewer            = 6;   // Number of records to allocate to each reviewer (before QC)
-    $max_initial_allocation_per_version         = floor($initial_qc_per_reviewer/ count($versions));    // Max number of records for any given version to be allocated ( use 1/2 of $initial_allocation for 2 versions)
     $initial_qc_per_reviewer                    = 2;    // Number of version 1 records to QC per reviewer
+    $max_initial_allocation_per_version         = floor($initial_qc_per_reviewer/ count($versions));    // Max number of records for any given version to be allocated ( use 1/2 of $initial_allocation for 2 versions)
     //$object_start                               = 1000; // Just a starting number - could be anything
     $object_count                               = 15;   // How many different objects to use (will end up record count =  $versions * $object_count + QC
 }
 
 
 
-
 // Generate objects from GCP bucket
 $object_names =  array_slice( $bucket_contents, 0, min($object_count, count($bucket_contents)) );
+
 if (count($bucket_contents) > $object_count) {
     $alert[] = "You are taking a subset of the total bucket objects (" . count($bucket_contents) . ") because your object_count setting is less (" . $object_count . ")";
 } else {
@@ -306,6 +424,7 @@ foreach ($results as $reviewer => $data) {
         );
     }
 }
+$total_assigned = $i;
 foreach ($unassigned['records'] as $object) {
     $i++;
     list($object_name, $object_version) = getNameVersion($object);
@@ -317,32 +436,70 @@ foreach ($unassigned['records'] as $object) {
     );
 }
 
-//$HtmlPage = new HtmlPage();
-//$HtmlPage->PrintHeaderExt();
 
-require_once APP_PATH_DOCROOT . 'ProjectGeneral/header.php';
+$summary = [];
+$summary[] = "Bucket objects in " . $bucket_prefix . " of " . $bucket_name . " = " . count($bucket_contents);
+$summary[] = "Reviewers (" . count($reviewers) . ") = " . implode(",", $reviewers);
+$summary[] = "Versions (" . count($versions) . ") = " . implode(",", $versions);
+$summary[] = "Bucket objects used = " . $object_count;
+$summary[] = "Initial records created (i.e. object-versions) = " . count($random_objects);
+$summary[] = "Initial QC per reviewer = " . $initial_qc_per_reviewer;
+$summary[] = "Total records created = " . count($rows);
+$summary[] = "Initial allocation per reviewer = " . $initial_allocation_per_reviewer;
+$summary[] = "Max initial allocations of version 1 per reviewer = " . $max_initial_allocation_per_version;
+$summary[] = "Number of records assigned = " . $total_assigned;
+$summary[] = "Number of records unassigned = " . count($unassigned['records']);
 
-//echo "<h4>CSV for Import <span class='badge badge-primary'>" . count($rows) . " rows</span></h4>";
-//echo "<pre>" . arrayToCsv($rows, true) . "</pre>";
+
+REDCap::logEvent("EKG Recordset Creator Run", implode("\n", $summary));
 
 ?>
+    <hr>
+    <h4>Results Summary:</h4>
+    <pre><?php echo implode("\n", $summary) ?></pre>
+    <hr>
+<?php
 
-<div id="loading">
-    Loading (this can take a while...)
-</div>
-<div id="results" style="display:none;">
+    // ALERTS
+    if (!empty($alert)) {
+        ?>
+        <hr>
+        <h5>Alerts <span class='badge badge-secondary'><?php echo count($alert) ?></span>
+            <button class="btn btn-xs btn-primary" type="button" data-toggle="collapse" data-target="#collapseAlert" aria-expanded="false" aria-controls="collapseAlert">
+                Toggle Alerts
+            </button>
+        </h5>
+        <div class="collapse show" id="collapseAlert">
+            <pre><?php echo print_r($alert,true) ?></pre>
+        </div>
+        <?php
+    }
 
-    <?php
+
+    // MISSING
+    if (!empty($missing)) {
+        ?>
+        <hr>
+        <h5>Missing Bucket Items <span class='badge badge-secondary'><?php echo count($missing) ?></span>
+            <button class="btn btn-xs btn-primary" type="button" data-toggle="collapse" data-target="#collapseMissing" aria-expanded="false" aria-controls="collapseMissing">
+                Toggle Missing Bucket Items
+            </button>
+        </h5>
+        <div class="collapse show" id="collapseMissing">
+            <pre><?php echo print_r($missing,true) ?></pre>
+        </div>
+        <?php
+    }
 
 
     // CSV
     if (!empty($rows)) {
         ?>
-        <h4>CSV For Import <span class='badge badge-secondary'><?php echo count($rows) ?> rows </span>
+        <h5>CSV For Import <span class='badge badge-secondary'><?php echo count($rows) ?> rows </span>
             <button class="btn btn-xs btn-primary" type="button" data-toggle="collapse" data-target="#collapseCSV" aria-expanded="false" aria-controls="collapseCSV">
-                Show CSV
+                Toggle CSV
             </button>
-        </h4>
+        </h5>
         <div class="collapse" id="collapseCSV">
             <pre><?php echo arrayToCsv($rows, true) ?></pre>
         </div>
@@ -351,45 +508,32 @@ require_once APP_PATH_DOCROOT . 'ProjectGeneral/header.php';
 
 
 
-    // ALERTS
-    if (!empty($alert)) {
+    // BUCKET CONTENTS
+    if (!empty($bucket_contents)) {
         ?>
         <hr>
-        <h4>Alerts <span class='badge badge-secondary'><?php echo count($alert) ?></span>
-            <button class="btn btn-xs btn-primary" type="button" data-toggle="collapse" data-target="#collapseAlert" aria-expanded="false" aria-controls="collapseAlert">
-                Show Alerts
+        <h5>Bucket Items <span class='badge badge-secondary'><?php echo count($bucket_contents) ?></span> files
+            <button class="btn btn-xs btn-primary" type="button" data-toggle="collapse" data-target="#collapseContents" aria-expanded="false" aria-controls="collapseContents">
+                Toggle Contents
             </button>
-        </h4>
-        <div class="collapse" id="collapseAlert">
-            <pre><?php echo print_r($alert,true) ?></pre>
+        </h5>
+        <div class="collapse" id="collapseContents">
+            <pre><?php echo implode("\n", $bucket_contents) ?></pre>
         </div>
         <?php
     }
 
-    // MISSING
-    if (!empty($missing)) {
-        ?>
-        <hr>
-        <h4>Missing Bucket Items <span class='badge badge-secondary'><?php echo count($missing) ?></span>
-            <button class="btn btn-xs btn-primary" type="button" data-toggle="collapse" data-target="#collapseMissing" aria-expanded="false" aria-controls="collapseMissing">
-                Show Missing Bucket Items
-            </button>
-        </h4>
-        <div class="collapse" id="collapseMissing">
-           <pre><?php echo print_r($missing,true) ?></pre>
-        </div>
-        <?php
-    }
 
     // DEBUG
     if (!empty($debug)) {
         ?>
+
         <hr>
-        <h4>Debug <span class='badge badge-secondary'><?php echo count($debug) ?></span>
+        <h5>Debug <span class='badge badge-secondary'><?php echo count($debug) ?></span>
             <button class="btn btn-xs btn-primary" type="button" data-toggle="collapse" data-target="#collapseDebug" aria-expanded="false" aria-controls="collapseDebug">
                 Show Debug
             </button>
-        </h4>
+        </h5>
         <div class="collapse" id="collapseDebug">
             <pre><?php echo print_r($debug,true) ?></pre>
         </div>
@@ -398,12 +542,3 @@ require_once APP_PATH_DOCROOT . 'ProjectGeneral/header.php';
 
     ?>
 </div>
-
-<script>
-    $(document).ready(function() {
-        $('#loading').hide();
-        $('#results').show();
-    });
-</script>
-
-<?php
